37c8dc0775ac0b78be8daf126c9c7bb4bd921adb
yuancc <yuancc@163.com>
2020-09-05 16:36:18 +0800

    新增笔记
    Signed-off-by: yuancc <yuancc@163.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -217,7 +217,7 @@ module.exports = {
           // 模型名称(default: hibiki)>>>取值请参考：
           // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
           // https://zhousiwei.gitee.io/live2d-widget.js/examples/index.html
-          model: 'haru/02',
+          model: 'hibiki',
           display: {
             position: "left", // 显示位置：left/right(default: 'right')
             width: 135, // 模型的长度(default: 135)
