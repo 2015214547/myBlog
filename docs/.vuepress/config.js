cf359c5cdd23b4f2694cf101e9153048a404787b
yuancc <yuancc@163.com>
2020-10-14 21:04:12 +0800

    梳理笔记
    Signed-off-by: yuancc <yuancc@163.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -73,7 +73,7 @@ module.exports = {
     blogger: { // 博主信息，显示在首页侧边栏
       avatar: '/img/mine.jpg',
       name: '诗酒趁年华',
-      slogan: '放下苦痛，才能沐浴阳光'
+      slogan: '穷且益坚，不坠青云之志'
     },
     social: { // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
