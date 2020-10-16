161f03f3e9c4a1f664f195051ace2ebd0ff2c64c
yuancc <yuancc@163.com>
2020-10-16 17:26:22 +0800

    新增笔记
    Signed-off-by: yuancc <yuancc@163.com>

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,9 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+# curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.yuanchengcheng.vip&token=f7FLLxhNr4Ugad8Y"
+
 
 rm -rf urls.txt # 灭迹
